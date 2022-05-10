d7d325fecfbdb16c0f029b0407953dbe0b42300a
Sergi Castillo Tiñena <sergicastillo@hotmail.es>
2022-05-10 17:04:53 +0200

    Dia4

--- Views/index.php
+++ Views/index.php
@@ -20,7 +20,7 @@ $user =  !empty($_SESSION["username"]) ? htmlspecialchars($_SESSION["username"])
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Gimnasio</title>    
-    <link rel="stylesheet" href="../src/css/styles.css">
+    <link rel="stylesheet" href="src/css/styles.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
@@ -29,7 +29,7 @@ $user =  !empty($_SESSION["username"]) ? htmlspecialchars($_SESSION["username"])
         <header>
             <div class="row">                
                 <div class="col-md-1">
-                    <img id="logo" src="../images/logotransparente.png" width="170" height="170">
+                    <img id="logo" src="images/logotransparente.png" width="170" height="170">
                 </div>
                 <div class="col-md-1"></div>
                 <div class="col-md-1 top">
@@ -57,7 +57,7 @@ $user =  !empty($_SESSION["username"]) ? htmlspecialchars($_SESSION["username"])
                             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                                 <li><a class="dropdown-item" href="Views/welcome.php">Panel de control</a></li>
                                 <li><a class="dropdown-item" href="">Cesta</a></li>
-                                <li><a class="dropdown-item" href="../src/php/cerrar.php">Cerrar sesión</a></li>
+                                <li><a class="dropdown-item" href="src/php/cerrar.php">Cerrar sesión</a></li>
                             </ul>
                         </div>
                     <?php endif ?>
@@ -95,10 +95,10 @@ $user =  !empty($_SESSION["username"]) ? htmlspecialchars($_SESSION["username"])
             </div>
             <div class="row">
                 <div class="col-md-12">
-                    <a href="#instalaciones" title=""><img id="scroll" src="../images/main/scroll.png"></a>                
+                    <a href="#instalaciones" title=""><img id="scroll" src="images/main/scroll.png"></a>                
                 </div>
                 <div class="col-md-12">
-                    <a href="#top"><img id="up" src="../images/main/up.png"></a>
+                    <a href="#top"><img id="up" src="images/main/up.png"></a>
                 </div>
             </div>
         </header>
@@ -117,13 +117,13 @@ $user =  !empty($_SESSION["username"]) ? htmlspecialchars($_SESSION["username"])
             </div>
             <div class="col-md-1"></div>
             <div class="col-md-12 col-lg-6">
-                <img id="instalacionesimg" src="../images/main/gym1.jpg">
+                <img id="instalacionesimg" src="images/main/gym1.jpg">
             </div>        
         </div>
         <div class="row tienda">
             <div class="col-md-1"></div>
             <div class="col-md-12 col-lg-6">
-                <img id="tiendaimg" src="../images/main/tienda.jpg">
+                <img id="tiendaimg" src="images/main/tienda.jpg">
             </div>
             <div class="col-lg-4 col-md-12">
                 <h1 id="tiendah1">Descubre nuestra tienda</h1>
@@ -293,5 +293,5 @@ $user =  !empty($_SESSION["username"]) ? htmlspecialchars($_SESSION["username"])
 </footer>
 
 </body>
-<script type="text/javascript" src="../src/scripts/script.js"></script>
+<script type="text/javascript" src="src/scripts/script.js"></script>
 </html>
