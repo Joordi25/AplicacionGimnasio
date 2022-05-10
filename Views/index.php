035463b3ccdb6068cbe7a500c0bb68956e7ce5ba
Joordi25 <jordibraso9900@gmail.com>
2022-05-10 19:13:56 +0200

    g

--- Views/index.php
+++ Views/index.php
@@ -21,6 +21,7 @@ $user =  !empty($_SESSION["username"]) ? htmlspecialchars($_SESSION["username"])
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Gimnasio</title>    
     <link rel="stylesheet" href="../src/css/styles.css">
+    <link rel="shortcut icon" href="../src/images/Logo.png" type="image/x-icon">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
