da03002d496f7eb3f576b1d026c10553880e7249
IvanMarBae <ivanmarbae@campus.monlau.com>
2022-05-10 16:59:47 +0200

    a

--- Views/index.php
+++ Views/index.php
@@ -107,12 +107,13 @@ $user =  !empty($_SESSION["username"]) ? htmlspecialchars($_SESSION["username"])
             <div class="col-md-1"></div>
             <div class="col-lg-4 col-md-12">
                 <h1 id="instalacionesh1">Descubre nuestras instalaciones</h1>
-                <p id="instalacionesp">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p id="instalacionesp">Actualmente contamos con unas instalaciones modernas y eficientes, contando con todo el material 
+                    para poder llevar acabo entrenos y sesiones programadas y privadas. Disponemos de varias salas de entreno para que nuestros
+                    clientes puedan desarrollar sus ejercicios con gran capacidad y de facil accessibilidad. Tambien cabe añadir que tanto zonas de maquinas y 
+                    salas de sesiones grupales, constan de material de alta calidad, con el que podamos satisfacer todas las necesidades de nuestros clientes.
+                    Si quieres visitar nuestro centro no dudes en contactarnos.
+
+                </p>
                 <a href="" id="instalacionesa">Conoce nuestras tarifas</a>
             </div>
             <div class="col-md-1"></div>
