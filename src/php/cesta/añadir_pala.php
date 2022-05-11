6d592c4bc3fbdc272a64803078fe6d24ddeed9e6
Joordi25 <jordibraso9900@gmail.com>
2022-05-11 19:07:30 +0200

    f

--- src/php/cesta/añadir_pala.php
+++ src/php/cesta/añadir_pala.php
@@ -9,37 +9,45 @@ require_once "../config.php";
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <title>Añadir Producto</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="../../../public/css/css.css">
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="../../css/LoginStyle.css" rel="stylesheet">
+    <!--<script src="../src/JavaScript/Rergister.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>  or use local jquery
+    <script src="../src/libs/jqBootstrapValidation.js"></script>
+    <script src="../src/libs/validate.min.js"></script>-->
+    <title>Login Zeus</title>
     <link rel="icon" href="../images/favicon.png">
 </head>
 
 <body>
-    <div class="login-page">
-        <div class="form">
-            <form action="insertar.php" method="post" class="login-form" enctype="multipart/form-data">
-                <div class="form-group">
-                    <label>Nombre de la pala</label>
-                    <input type="text" name="name" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label>Precio</label>
-                    <input type="number" name="price" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label>Imagen</label>
-                    <input type="file" name="img" accept="image/png, image/jpeg">
-                </div>
-                
-                <div class="form-group">
-                    <input type="submit" href="..\Index.php" value="Añadir" id="iniciar">
-                </div>
-                <p><a href="inicio_admin.php" class="btn btn-primary">Volver atrás</a></p>
-            </form>
-        </div>
+    <img src="../../images/Logo.png">
+    <div class="container" style="width: 400px;">
+    <form action="insertar.php" method="post" class="login-form" enctype="multipart/form-data">
+
+            <label for="username">Nombre del producto</label><br>
+            <input class="form-control" type="text" id="username" name="name" required><br><br>
+
+            <label for="password">Precio</label><br>
+            <input class="form-control" type="password" name="price" id="password" required><br><br>
+
+            <label for="password">Imagen</label><br>
+            <input type="file" name="img" accept="image/png, image/jpeg"> <br> <br>
+
+            <button type="submit" class="btn btn-outline-warning" id="iniciar"  href="..\Index.php">Añadir Producto</button><br><br><br>
+            
+            
+            <a style="color: yellow" href="inicio_admin.php">Atrás</a><br><br>
+        </form>
     </div>
+
+
+
+
+
 </body>
 
 </html>
