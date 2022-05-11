9ca6e1e8c0c76cae8f846096408e5f9b3995e58b
Joordi25 <jordibraso9900@gmail.com>
2022-05-11 19:20:10 +0200

    d

--- src/php/cesta/inicio_admin.php
+++ src/php/cesta/inicio_admin.php
@@ -19,7 +19,8 @@ $user =  !empty($_SESSION["username"]) ? htmlspecialchars($_SESSION["username"])
 	<meta charset="utf-8">
 	<title>Productos</title>
 	<link rel="icon" href="../images/favicon.png">
-	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link href="../../css/StyleProductos.css" rel="stylesheet">
 	<style>
 		.product_image {
