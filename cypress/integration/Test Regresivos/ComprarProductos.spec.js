14c43e9b1a0d399fe2255a16ba72c41460b7e354
Joordi25 <jordibraso9900@gmail.com>
2022-05-11 16:21:19 +0200

    Create ComprarProductos.spec.js

---
+++ cypress/integration/Test Regresivos/ComprarProductos.spec.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress" />
+
+
+describe('Iniciar Sesión en la web', () => {
+    beforeEach(() => {
+
+        cy.visit('http://localhost/AplicacionGimnasio/Views/index.php')
+        cy.get(':nth-child(10) > .menu').click()
+        cy.wait(2000)
+        cy.get('#username').type('test')
+        cy.get('#password').type('12345')
+        cy.get('#iniciar').click()
+        cy.wait(2000)
+    })
+
+
+
+    it('Ir a la tienda y ecoger varios productos', () => {
+
+        cy.get(':nth-child(5) > .menu').click({ force: true })
+        cy.get(':nth-child(1) > .panel > .panel-body > .product_footer > .pull-right > .btn').click()
+        cy.get(':nth-child(2) > .panel > .panel-body > .product_footer > .pull-right > .btn').click()
+        cy.get('li > a').click()
+        cy.get('[href="clear_cart.php"]').click()
+        cy.wait(2000)
+        cy.get(':nth-child(1) > .panel > .panel-body > .product_footer > .pull-right > .btn').click()
+        cy.get(':nth-child(2) > .panel > .panel-body > .product_footer > .pull-right > .btn').click()
+        cy.get('li > a').click()
+        cy.get('[href="compra/pagar.php"]').click()
+
+        cy.get('.number').type('5351 2040 2043 1013')
+        cy.get('.inputname').type('test test test')
+        cy.get('.expire').type('1027')
+        cy.get('.ccv').type('927')
+
+
+    })
+
+
+
+});
