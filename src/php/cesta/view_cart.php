dec147bbb4cd517ef819ab3143979602707f6fac
TheNaviam <ivan.aranda.martinez.11@gmail.com>
2022-05-13 18:23:25 +0200

    cesta

--- src/php/cesta/view_cart.php
+++ src/php/cesta/view_cart.php
@@ -9,7 +9,7 @@ $user =  !empty($_SESSION["username"]) ? htmlspecialchars($_SESSION["username"])
 <head>
 	<meta charset="utf-8">
 	<title>Carrito</title>
-    <link rel="stylesheet" href="../../../src/css/StyleProductos.css">
+    <link rel="stylesheet" href="../../../src/css/CestaStyles.css">
 	<link rel="icon" href="\AplicacionGimnasio\images\icon.png">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -65,31 +65,9 @@ $user =  !empty($_SESSION["username"]) ? htmlspecialchars($_SESSION["username"])
 
 	</div>
 	<div class="container">
-		<nav class="navbar navbar-default">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<p class="navbar-brand"> Esta es lo que tienes en la cesta, <?php echo $user; ?></p>
-				</div>
-
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav">
-						<!-- left nav here -->
-					</ul>
-					<ul class="nav navbar-nav navbar-right">
-						<li class="active"><a href="view_cart.php"><span class="badge"><?php echo count($_SESSION['cart']); ?></span> Carrito <span class="glyphicon glyphicon-shopping-cart"></span></a></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-		<h1 class="page-header text-center">Detalles de la cesta</h1>
+		<h1 class="page-header text-center" style="color: yellow;">Detalles de la cesta</h1>
 		<div class="row">
-			<div class="col-sm-8 col-sm-offset-2">
+			<div class="col-sm-8 col-sm-offset-2" style="margin-left: auto; margin-right: auto;">
 				<?php
 				if (isset($_SESSION['message'])) {
 				?>
@@ -102,7 +80,7 @@ $user =  !empty($_SESSION["username"]) ? htmlspecialchars($_SESSION["username"])
 
 				?>
 				<form method="POST" action="save_cart.php">
-					<table class="table table-bordered table-striped">
+					<table class="table table-bordered table-striped" style="color: white; background-color: rgba(148, 144, 144, 0.5);">
 						<thead>
 							<th></th>
 							<th>Nombre</th>
@@ -143,7 +121,7 @@ $user =  !empty($_SESSION["username"]) ? htmlspecialchars($_SESSION["username"])
 							} else {
 								?>
 								<tr>
-									<td colspan="4" class="text-center">No hay nada en el carrito</td>
+									<td style="color: white;" colspan="4" class="text-center">No hay nada en el carrito</td>
 								</tr>
 							<?php
 							}
@@ -155,10 +133,8 @@ $user =  !empty($_SESSION["username"]) ? htmlspecialchars($_SESSION["username"])
 							</tr>
 						</tbody>
 					</table>
-					<a href="inicio.php" class="btn btn-primary"><span class="glyphicon glyphicon-arrow-left"></span> Atrás</a>
-					<a href="../../../Views/index.php" class="btn btn-success"> Inicio</a>
 					<a href="clear_cart.php" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Limpiar carrito</a>
-					<a href="compra/pagar.php" class="btn btn-success"><span class="glyphicon glyphicon-check"></span> Comprar</a>
+					<a href="compra/pagar.php" class="btn btn-warning"><span class="glyphicon glyphicon-check"></span> Comprar</a>
 				</form>
 			</div>
 		</div>
