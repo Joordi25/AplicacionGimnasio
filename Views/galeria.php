ffde78751bcd41c88468344b3dde99dd775b2741
TheNaviam <ivan.aranda.martinez.11@gmail.com>
2022-05-18 17:05:57 +0200

    a

--- Views/galeria.php
+++ Views/galeria.php
@@ -17,7 +17,7 @@ $user =  !empty($_SESSION["username"]) ? htmlspecialchars($_SESSION["username"])
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Galería</title>    
+    <title>Sobre nosotros</title>    
     <link rel="stylesheet" href="../src/css/galeriastyles.css">
     <link rel="shortcut icon" href="../images/icon.png" type="image/x-icon">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
