751d7feef477345babf16dac07b0e50e66b4bd62
TheNaviam <ivan.aranda.martinez.11@gmail.com>
2022-05-30 18:47:34 +0200

    a

--- src/php/recuperar.php
+++ src/php/recuperar.php
@@ -3,29 +3,24 @@ include('config.php');
 
 $Correo = $_POST['txtcorreo'];
 
-$queryusuario = mysqli_query($link,"SELECT * FROM users WHERE Correo = '$Correo'");
-$nr = mysqli_num_rows($queryusuario); 
-if ($nr == 1)
-{
-$mostrar = mysqli_fetch_array($queryusuario); 
-$enviarpass = $mostrar['password'];
+$queryusuario = mysqli_query($link, "SELECT * FROM users WHERE Correo = '$Correo'");
+$nr = mysqli_num_rows($queryusuario);
+if ($nr == 1) {
+	$mostrar = mysqli_fetch_array($queryusuario);
+	$enviarpass = $mostrar['password'];
 
-$paracorreo = $Correo;
-$titulo = "Recuperar contraseña";
-$mensaje = $enviarpass;
-$tucorreo = "sergicastillom7@hotmail.com";
+	$paracorreo = $Correo;
+	$titulo = "Recuperar contraseña";
+	$mensaje = $enviarpass;
+	$tucorreo = "jordibraso9900@gmail.com";
 
-if(mail($paracorreo,$titulo,$mensaje,$tucorreo))
-{
-	echo "<script> alert('Contraseña enviado');window.location= '../../Views/LoginView.php' </script>";
-}else
-{
-	echo "<script> alert('Error');window.location= '../../Views/OlvidarView.php' </script>";
-}
-}
-else
-{
+	if (mail($paracorreo, $titulo, $mensaje, $tucorreo)) {
+		echo "<script> alert('Contraseña enviado');window.location= '../../Views/LoginView.php' </script>";
+	} else {
+		die();
+		echo "<script> alert('Error');window.location= '../../Views/OlvidarView.php' </script>";
+	}
+} else {
 	echo "<script> alert('Este correo no existe');window.location= '../../Views/OlvidarView.php' </script>";
 }
 /*VaidrollTeam*/
-?>
