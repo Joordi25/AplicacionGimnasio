3121e1d40226e1ead142ede23ffedea9fb35b8ce
Joordi25 <jordibraso9900@gmail.com>
2022-05-31 16:25:46 +0200

    d

--- src/php/cesta/añadir_pala.php
+++ src/php/cesta/añadir_pala.php
@@ -15,10 +15,6 @@ require_once "../config.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="../../css/LoginStyle.css" rel="stylesheet">
-    <!--<script src="../src/JavaScript/Rergister.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>  or use local jquery
-    <script src="../src/libs/jqBootstrapValidation.js"></script>
-    <script src="../src/libs/validate.min.js"></script>-->
     <title>Añadir Producto</title>
     <link rel="icon" href="../../../images/icon.png">
 </head>
